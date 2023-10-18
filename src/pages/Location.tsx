65f456d9b403133c614013acb07737d1d59a2540
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-10-18 22:22:27 +0000

    status component completed

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -4,6 +4,9 @@ import { useSocket } from "../context/socket";
 import Map from "../components/Element/Map";
 import { GeolocationPosition, socket_status, room_status } from "../types";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import Status from "../components/Element/Status";
+import StatusPanel from "../components/Element/StatusPanel";
+
 
 function Location() {
   const { roomId } = useParams();
@@ -69,14 +72,68 @@ function Location() {
         setSocketStatus("disconnected");
       });
     }
-  });
+  },[socket]);
 
   return (
     <>
     <section className="pb-3">
       <article className="bg-slate-600 rounded-md p-3 flex flex-wrap gap-3 justify-between items-center w-full">
-        
+        <Status locationStatus={null} socketStatus={socketStatus} />
+
+        {
+          position && (
+            <div className="flex gap-2 justify-end text-gray-200">
+              <p className="font-bold text-sm">Lat: <span className="text-lg font-bold">{position.lat}</span> </p>
+              <p className="font-bold text-sm">Lng: <span className="text-lg font-bold">{position.lng}</span> </p>
+            </div>
+          )
+        }
       </article>
+    </section>
+    {
+      roomStatus==="joined" && (
+        <section>
+          {
+            position && (
+              <div className="bg-gray-200 rounded-md overflow-hidden">
+                <Map location={position} />
+              </div>
+            )
+          }
+        </section>
+      )
+    }
+    <section>
+      {
+        socketStatus==="connecting"&&(
+          <article className="mt-5">
+            <StatusPanel title="Connecting to Server" subtitle="Please wait..." status="loading" />
+          </article>
+        )
+      }
+      {
+        socketStatus==="error"&&(
+          <article className="mt-5">
+            <StatusPanel title="Failed to connect to server" subtitle="Please try again later" status="error" />
+          </article>
+        )
+      }
+      {
+        roomStatus==="not-exist"&&(
+          <article className="mt-5">
+            <StatusPanel title="Room does not exist" subtitle="Enter the correct URL" status="error" />
+          </article>
+        )
+      }
+
+{
+        roomStatus==="joined"&&(
+          <article className="mt-5">
+            <BsFillArrowLeftCircleFill size={20} className="cursor-pointer" onClick={()=>window.open('/','_self')} />
+            <p className="text-md font-semibold">Back</p>
+          </article>
+        )
+      }
     </section>
     </>
   );
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,5 @@
 import { Socket } from "socket.io-client";
 
-
 export type GeolocationPosition = {
   lat: number;
   lng: number;
@@ -10,9 +9,18 @@ export type location_status = "accessed" | "denied" | "unknown" | "error";
 
 export type room_status = "unknown" | "joined" | "not-exist";
 
-export type socket_status = "connecting" | "connected" |"disconnected" | "error";
+export type socket_status =
+  | "connecting"
+  | "connected"
+  | "disconnected"
+  | "error";
 
 export type SocketContextType = {
   socket: Socket | null;
   connectSocket: () => void;
+};
+
+export type StatusProps = {
+  locationStatus: location_status | null;
+  socketStatus: socket_status | null;
 };
